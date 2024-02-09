326e0571817061d470835c6213b36fdd8d10620e
Anzelah <jepkoechanzellah@gmail.com>
2024-02-09 21:04:42 +0000

    implement the fifo caching eviction policy

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""create a basic caching"""
+
+from queue import Queue
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Implement a fifo caching replacement policy
+    """
+    def __init__(self):
+        super().__init__()
+        self.list = Queue()
+
+    def put(self, key, item):
+        """Discard items when caching is full using FIFO technique
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            oldest = self.list.get()
+            del self.cache_data[oldest]
+            print("DISCARD: {}" .format(oldest))
+
+        self.cache_data[key] = item
+        self.list.put(key)
+
+    def get(self, key):
+        """Return a value linked to a key in dictionary
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
