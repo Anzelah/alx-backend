7b9877c7d41f4690039b98184c7ada3f20984187
Anzelah <jepkoechanzellah@gmail.com>
2024-02-10 09:52:35 +0000

    implemented with lists instead of lifoqueue

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -5,37 +5,32 @@ from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
+    """Implement a LIFO caching eviction system
     """
-    Implement a caching system which uses the lifo caching replacement method
-    """
-
     def __init__(self):
         """Initiliaze our instances with their initial attributes
-        Returns:
-            LifoCache: A new instance of the LifoCache class.
         """
         super().__init__()
         self.list = []
 
     def put(self, key, item):
         """Discard items when caching is full using FIFO technique
-        Returns the cache but with some items evicted and replaced
         """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            newest = self.list.get()
-            del self.cache_data[newest]
+            newest = self.list[-1]
             print("DISCARD: {}" .format(newest))
+            del self.cache_data[newest]
+            del newest
 
         self.cache_data[key] = item
-        self.list.put(key)
+        self.list.append(key)
 
     def get(self, key):
         """Return a value linked to a key in dictionary
-        First check if the key is valid and if its indeed in the cache
         """
         if key is None or key not in self.cache_data:
             return None
