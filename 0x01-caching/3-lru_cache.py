77743c5a74a6ffc7ebd1635822437f157407316e
Anzelah <jepkoechanzellah@gmail.com>
2024-02-10 11:10:20 +0000

    implementing LRU

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""The lru caching replacement technique
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LRUCache(BaseCaching):
+    """Implement a LRU caching eviction system
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Discard items using lru method when cache gets full
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            lru = self.cache_data.popitem(last=False)
+            print("DISCARD: {}" .format(lru[0]))
+
+    def get(self, key):
+        """Return value linked to a key in the dictinary
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
