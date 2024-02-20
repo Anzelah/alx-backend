3ebd6d2db1310b557b114a38164083f97a7beb7f
Anzelah <jepkoechanzellah@gmail.com>
2024-02-20 18:36:27 +0000

    get locale from request

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Flask babel"""
+
+from flask import Flask
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration class
+    """
+    LANGUAGES = ["en", "fr"]
+
+app = Flask(__name__)
+app.config.from_pyfile('mysettings.cfg')
+
+babel = Babel(app)
+babel.init_app(app)
+app.config['LANGUAGES'] = Config.LANGUAGES
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
