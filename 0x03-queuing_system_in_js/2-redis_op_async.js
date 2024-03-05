8b5ec2985578eb90f779616ea67b46d74dd65e1b
Anzelah <jepkoechanzellah@gmail.com>
2024-03-05 18:34:21 +0000

    use async await

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,32 @@
+import redis from 'redis';
+
+
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const client = redis.createClient(REDIS_PORT);
+
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server: ', err.message);
+});
+
+
+function setNewSchool(schoolName, value) {
+ client.SET(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+  await client.GET(schoolName, (err, reply) => {
+    if (err) {
+     console.error(err);
+     return;
+    }
+    console.log(reply);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
