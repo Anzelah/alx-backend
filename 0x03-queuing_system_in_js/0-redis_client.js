6aa1571fa29d5898bafd0c678a4a54ca1beadbe8
Anzelah <jepkoechanzellah@gmail.com>
2024-03-05 17:03:46 +0000

    conect server to client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,12 @@
+import redis from 'redis';
+
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const client = redis.createClient(REDIS_PORT);
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.error('Redis client not connected to the server: ',err.message);
+});
